2fb1509c699b3821483dea71bfa43068429e9d8e
xiaobai <419456942@qq.com>
2019-10-24 14:29:21 +0800

    日常提交

---
+++ imoocmanager/src/components/Header/index.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Row, Col } from 'antd'
+import './index.less'
+import Utils from '../../utils/utils'
+import axios from '../../axios'
+export default class Header extends React.Component{
+  state = {}
+  componentWillMount(){
+    this.setState({
+      userName : '小白'
+    })
+    setInterval(()=>{
+      let sysTime = Utils.formateDate(new Date().getTime());
+      this.setState({
+        sysTime
+      })
+    },1000)
+    this.getWeatherAPIData()
+  }
+  getWeatherAPIData(){
+    let city = '北京';
+    axios.jsonp({
+      url:'http://api.map.baidu.com/telematics/v3/weather?location='+encodeURIComponent(city)+'&output=json&ak=3p49MVra6urFRGOT9s8UBWr2'
+    }).then((res)=>{
+      console.log(res)
+      if(res.status === 'success'){
+        let data = res.results[0].weather_data[0];
+        this.setState({
+          dayPictureUrl:data.dayPictureUrl,
+          weather:data.weather
+        })
+      }
+    })
+  }
+  render(){
+    return (
+      <div className="header">
+        <Row className="header-top">
+          <Col span="24">
+            <span>欢迎，{this.state.userName}</span>
+            <a href="#">退出</a>
+          </Col>
+        </Row>
+        <Row className="breadcrumb">
+          <Col span="4" className="breadcrumb-title">
+            首页
+          </Col>
+          <Col span="20" className="weater">
+            <span className="date">{this.state.sysTime}</span>
+            <span className="weater-img">
+              <img src={this.state.dayPictureUrl} />
+            </span>
+            <span className="weater-detail">
+              {this.state.weather}
+            </span>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
+}
