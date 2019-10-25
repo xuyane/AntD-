ddc1cc4ebf57f2311b511fcc98cfb6e40dcb296b
xiaobai <419456942@qq.com>
2019-10-25 17:00:40 +0800

    日常提交

--- imoocmanager/src/axios/index.js
+++ imoocmanager/src/axios/index.js
@@ -1,4 +1,6 @@
 import JsonP from 'jsonp'
+import axios from 'axios'
+import { Modal } from 'antd'
 export default class Axios {
   static jsonp(options){
     return  new Promise((resolve,reject)=>{
@@ -12,5 +14,32 @@ export default class Axios {
         }
       })
     })
+  }
+  static ajax(options){
+    let baseApi = 'https://www.easy-mock.com/mock/5a7278e28d0c633b9c4adbd7/api';
+    // Promise里面会接收一个回调函数
+    return new Promise((resolve,reject)=>{
+      axios({
+        url:options.url,
+        method:'get',
+        baseURL:baseApi,
+        timeout:5000,
+        param:(options.data && options.data.params) || ''
+      }).then((response)=>{
+        if(response.status == '200'){
+          let res = response.data;
+          if(res.code == '0'){
+            resolve(res);
+          }else{
+            Modal.info({
+              title:'提示',
+              content:res.msg
+            })
+          }
+        }else{
+          reject(response.data)
+        }
+      })
+    });
   }
 }
